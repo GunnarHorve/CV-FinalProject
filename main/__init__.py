08ee35afa97c84521a66b2bd91611716852210a4
Max <mflevine@wpi.edu>
2016-11-14 18:52:45 -0500

    Example code

--- main/__init__.py
+++ main/__init__.py
@@ -1,1 +1,10 @@
+import cv2
+from matplotlib import pyplot as plt
+
+mat = cv2.imread('testimage.jpg',cv2.IMREAD_GRAYSCALE)
+col = cv2.imread('testimage.jpg')
+ret,thresh1 = cv2.threshold(mat,127,255,cv2.THRESH_BINARY)
+plt.imshow(thresh1, cmap='Greys')
+plt.xticks([]), plt.yticks([])
+plt.show()
 print("hello world")
