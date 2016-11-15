fe8481b97bb52f6d3f96486cebd4153e2b38d694
GunnarHorve <gunnarhorve@gmail.com>
2016-11-14 20:05:55 -0500

    gunnar system online

--- main/__init__.py
+++ main/__init__.py
@@ -1,10 +1,5 @@
 import cv2
-from matplotlib import pyplot as plt
 
-mat = cv2.imread('testimage.jpg',cv2.IMREAD_GRAYSCALE)
-col = cv2.imread('testimage.jpg')
-ret,thresh1 = cv2.threshold(mat,127,255,cv2.THRESH_BINARY)
-plt.imshow(thresh1, cmap='Greys')
-plt.xticks([]), plt.yticks([])
-plt.show()
-print("hello world")
+mat = cv2.imread('lena.png',cv2.CV_LOAD_IMAGE_COLOR)
+cv2.imshow("top kek",mat)
+cv2.waitKey(0)
