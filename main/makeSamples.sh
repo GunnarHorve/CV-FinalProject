672a10ed8f30171a5bf0463d836d45d2f6c1ecfe
GunnarHorve <gunnarhorve@gmail.com>
2016-11-30 16:17:54 -0500

    killed files

--- main/makeSamples.sh
+++ main/makeSamples.sh
@@ -1,2 +1,3 @@
 opencv_createsamples -img side.JPG -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1950
-
+opencv_createsamples -info info/info.lst -num 1950 -w 20 -h 20 -vec positives.vec
+opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1800 -numNeg 900 -numStages 10 -w 20 -h 20
