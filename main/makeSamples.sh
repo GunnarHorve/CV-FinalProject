08168fce068e773174a064298a747e305eff6007
mflevine <mflevine@users.noreply.github.com>
2016-12-02 18:43:30 -0500

    Update makeSamples.sh

--- main/makeSamples.sh
+++ main/makeSamples.sh
@@ -1,3 +1,3 @@
-opencv_createsamples -img side.JPG -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 0.5 -maxyangle 0.5 -maxzangle 0.5 -num 1950
+opencv_createsamples -img side.jpg -bg bg.txt -info info/info.lst -pngoutput info -maxxangle 1.5 -maxyangle 1.5 -maxzangle 1.5 -num 1950 -bgcolor 255 -bgthresh 30
 opencv_createsamples -info info/info.lst -num 1950 -w 20 -h 20 -vec positives.vec
-opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1800 -numNeg 900 -numStages 10 -w 20 -h 20
+opencv_traincascade -data data -vec positives.vec -bg bg.txt -numPos 1000 -numNeg 1000 -numStages 10 -w 20 -h 20
